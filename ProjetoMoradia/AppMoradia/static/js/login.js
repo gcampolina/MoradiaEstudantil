be78e13d45da780f527b1d2a8932f7ba4e91abdc
Gabriel Campolina <gcampolinap@gmail.com>
2025-04-29 22:21:06 -0300

    Responsividade Final

--- ProjetoMoradia/AppMoradia/static/js/login.js
+++ ProjetoMoradia/AppMoradia/static/js/login.js
@@ -10,3 +10,27 @@
         container.classList.remove("right-panel-active");
     });
 
+    
+
+    document.addEventListener('DOMContentLoaded', function () {
+        const showLoginBtn = document.getElementById('showLoginBtn');
+        const showSignupBtn = document.getElementById('showSignupBtn');
+        const signInContainer = document.getElementById('sign-in-container');
+        const signUpContainer = document.getElementById('sign-up-container');
+        
+        if (showLoginBtn && showSignupBtn && signInContainer && signUpContainer) {
+            // Botão para mostrar o formulário de login
+            showLoginBtn.addEventListener('click', function () {
+                signInContainer.style.display = 'block';  // Exibe o login
+                signUpContainer.style.display = 'none';   // Oculta o cadastro
+            });
+    
+            // Botão para mostrar o formulário de cadastro
+            showSignupBtn.addEventListener('click', function () {
+                signUpContainer.style.display = 'block';  // Exibe o cadastro
+                signInContainer.style.display = 'none';   // Oculta o login
+            });
+        } else {
+            console.error('Elementos não encontrados!');
+        }
+    });
